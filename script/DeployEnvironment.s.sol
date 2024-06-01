4c857a47228603446094ed68b744eb58cb14247b
malteish <malteish@corpus.io>
2024-06-02 00:00:46 +0200

    fix smart contract deployment script

--- script/DeployEnvironment.s.sol
+++ script/DeployEnvironment.s.sol
@@ -9,6 +9,7 @@ contract MyScript is Script {
     function run(address chain_fusion_canister_address) external {
         // the private key of the deployer is the first private key printed by running anvil
         uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        address deployerAddress = vm.addr(deployerPrivateKey);
         // we use that key to broadcast all following transactions
         vm.startBroadcast(deployerPrivateKey);
 
@@ -25,14 +26,15 @@ contract MyScript is Script {
             "TST"
         );
 
-        // mint 1000 tokens to the deployer
-        token.mint(address(this), 1000);
+        // mint a bunch of tokens to the deployer
+        uint256 aBunchOfTokens = 1e30;
+        token.mint(address(deployerAddress), aBunchOfTokens);
 
         // approve the recurringTransactions contract to spend a bunch of tokens
-        token.approve(address(recurringTransactions), 10000e18);
+        token.approve(address(recurringTransactions), aBunchOfTokens);
 
         // create a job that will send 0,3 tokens to the deployer every 20 seconds
-        recurringTransactions.createJob(
+        recurringTransactions.createJob{value: 0.01 ether}(
             20,
             0.3 ether, // assuming 18 decimals here
             address(2), // some memorable address
