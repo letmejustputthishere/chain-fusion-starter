62a5fd8f5bee52b12e54044f846290c8e8575929
malteish <malteish@corpus.io>
2024-06-01 23:34:04 +0200

    move script

--- script/createJob.s.sol
+++ script/createJob.s.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.20;
 
 import "../../../lib/forge-std/src/Script.sol";
-import "../Coprocessor.sol";
+import "../RecurringTransactions.sol";
 
-contract Start is Script {
+contract CreateJob is Script {
     uint256 accountPrivateKey;
     address accountAddress;
 
@@ -15,20 +15,17 @@ contract Start is Script {
 
     function run() public {
         // local testnet
-        Coprocessor coprocessor = Coprocessor(
+        RecurringTransactions recurringTransactions = RecurringTransactions(
             0x5FbDB2315678afecb367f032d93F642f64180aa3
         );
 
         // load contract bytecode
-        bytes memory bytecode = type(Coprocessor).creationCode;
+        bytes memory bytecode = type(RecurringTransactions).creationCode;
         console.log("Bytecode length: %d", bytecode.length);
 
         console.log("Calling newJob...");
         vm.startBroadcast();
-        // payable(address(0xDFcEB49eD21aE199b33A76B726E2bea7A72127B0)).transfer(
-        //     300 ether
-        // );
-        coprocessor.newJob{value: 0.1 ether}();
+        recurringTransactions.newJob{value: 0.1 ether}();
         vm.stopBroadcast();
     }
 }
