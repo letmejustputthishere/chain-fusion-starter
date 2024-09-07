8a4444dec06bf8628fde975a3c00b6abde2a4ca3
malteish <malteish@corpus.io>
2024-09-07 21:17:25 +0200

    fix smart contract test

--- test/RecurringTransactions.t.sol
+++ test/RecurringTransactions.t.sol
@@ -7,7 +7,7 @@ import "../contracts/ERC20MintableByAnyone.sol";
 
 contract RecurringTransactionsTest is Test {
     function testExecuteJob() public {
-        address trigger = address(1);
+        address trigger = address(123);
         address sender = address(2);
         address recipient = address(3);
 
@@ -22,23 +22,71 @@ contract RecurringTransactionsTest is Test {
 
         uint256 aBunchOfTokens = 1e30;
         token.mint(sender, aBunchOfTokens);
+        uint128 amountToTransfer = 0.3 ether;
+
+        // Give the sender 1 ETH
+        vm.deal(sender, 1 ether);
+
+        // store initial balances
+        uint256 initialRecipientTokenBalance = token.balanceOf(recipient);
 
         vm.startPrank(sender);
         token.approve(address(recurringTransactions), aBunchOfTokens);
-        recurringTransactions.createJob(
+        recurringTransactions.createJob{value: 0.1 ether}(
             20,
             10,
-            0.3 ether,
+            amountToTransfer,
             recipient,
             address(token)
         );
         vm.stopPrank();
 
-        console.log(
-            "RecurringTransactions address: ",
-            address(recurringTransactions)
+        // Check if tokens were transferred to the recipient
+        assertEq(
+            token.balanceOf(recipient) - initialRecipientTokenBalance,
+            amountToTransfer,
+            "Incorrect amount of tokens transferred to recipient"
         );
 
-        vm.stopBroadcast();
+        // Get the job details
+        (
+            uint64 storedPeriod,
+            uint64 storedNumberOfRemainingExecutions,
+            uint128 storedAmount,
+            uint256 storedLastExecutionTime,
+            address storedSender,
+            address storedRecipient,
+            address storedTokenAddress
+        ) = recurringTransactions.jobs(0);
+
+        // Check if job details are correctly stored
+        assertEq(storedPeriod, 20, "Incorrect delay stored");
+        assertEq(
+            storedNumberOfRemainingExecutions,
+            9,
+            "Incorrect remaining executions stored"
+        );
+        assertEq(storedAmount, amountToTransfer, "Incorrect amount stored");
+        assertEq(storedSender, sender, "Incorrect sender stored");
+        console.log("jobRecipient", storedRecipient);
+        console.log("recipient", recipient);
+        assertEq(storedRecipient, recipient, "Incorrect recipient stored");
+        assertEq(
+            storedTokenAddress,
+            address(token),
+            "Incorrect token address stored"
+        );
+        assertEq(
+            storedLastExecutionTime,
+            1,
+            "Incorrect last execution time stored"
+        );
+
+        // Check if ETH was sent to the executor (trigger)
+        assertEq(
+            trigger.balance,
+            0.1 ether,
+            "Incorrect amount of ETH transferred to executor"
+        );
     }
 }
